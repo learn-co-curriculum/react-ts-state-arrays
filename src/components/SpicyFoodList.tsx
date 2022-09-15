a8111fb45c840efc84c54c22e3c85b8a17889308
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-09-15 12:02:40 -0700

    update lesson with ts notes

--- src/components/SpicyFoodList.tsx
+++ src/components/SpicyFoodList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,16 +1,18 @@
-const spicyFoods = [
+interface Food {
+  id?: number;
+  name: string;
+  cuisine: string;
+  heatLevel: number;
+}
+
+const spicyFoods: Food[] = [
   { id: 1, name: "Buffalo Wings", cuisine: "American", heatLevel: 3 },
   { id: 2, name: "Mapo Tofu", cuisine: "Sichuan", heatLevel: 6 },
 ];
 
 let nextId = 3;
 
-const newSpicyFoods: {
-  id?: number;
-  name: string;
-  cuisine: string;
-  heatLevel: number;
-}[] = [
+const newSpicyFoods: Food[] = [
   { name: "Green Curry", cuisine: "Thai", heatLevel: 9 },
   { name: "Enchiladas", cuisine: "Mexican", heatLevel: 2 },
   { name: "5 Alarm Chili", cuisine: "American", heatLevel: 5 },
@@ -19,7 +21,7 @@ const newSpicyFoods: {
 // Get one random spicy food from the array
 function getNewRandomSpicyFood() {
   const index = Math.floor(Math.random() * newSpicyFoods.length);
-  const newSpicyFood = { ...newSpicyFoods[index] };
+  const newSpicyFood: Food = { ...newSpicyFoods[index] };
   newSpicyFood.id = nextId;
   nextId++;
   return newSpicyFood;
